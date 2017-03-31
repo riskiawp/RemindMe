3fd2c464a22c1028124d7665dbb33b1cc2d5f18c
riskiawp <riskianandawp@gmail.com>
2017-03-31 08:35:42 +0700

    Final commit

---
+++ app/src/androidTest/java/id/sch/smktelkom_mlg/project2/xirpl51722262832/remindme/ApplicationTest.java
@@ -0,0 +1,13 @@
+
+
+package id.sch.smktelkom_mlg.project2.xirpl51722262832.remindme;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
